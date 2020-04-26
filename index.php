640e32c037ffcb03ebdae3077dcdffc43da0ea82
asanotomoki <mirai20190820@gmail.com>
2020-04-26 13:26:37 +0900

    linebot

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-
+require_once("./phpQuery-onefile.php");
+$html = file_get_contents("https://www.ei-navi.jp/dictionary/content/dog/");
+echo phpQuery::newDocument($html)->find(".en")->text();
 ?>
