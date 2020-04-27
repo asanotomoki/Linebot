bb47f736fb94e8d02ca9a7d77ad66dbe9ef7ab34
asanotomoki <mirai20190820@gmail.com>
2020-04-27 17:11:52 +0900

    修正13

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ $json_object = json_decode($json_string);
 //取得データ
 $replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
 $message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ*/
-$message_txt = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
-$message_text = mb_strtolower($message_txt);
+$message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
+//$message_text = mb_strtolower($message_txt);
 //メッセージタイプが「text」以外のときは何も返さず終了
 //if($message_type != "text") exit;
 //返信メッセージ
